98f69a05cfa5f76c1336b7f4fccdd532193e4db4
PhiHung1503 <phihung15032001@gmail.com>
2022-11-10 00:19:20 +0700

    update component/user-form.component.js file

--- src/component/user-form.component.js
+++ src/component/user-form.component.js
@@ -3,10 +3,23 @@ export default class UserForm extends Component {
     render() {
         return (
             <form>
-                {/* <div className="form-group">
+                <div className="form-group">
                     <label>Name</label>
+                    <input type="text" className="form-control" />
+                    <small className="text-danger">Name is aria-required.</small>
+                </div>
 
-                </div> */}
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <button type="submit" className="btn btn-block btn-danger">Create User</button>
             </form>
         );
     }
