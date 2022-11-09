14134a8d2f2e5ab38e4183d391f866b85f789486
PhiHung1503 <phihung15032001@gmail.com>
2022-11-10 00:13:33 +0700

    create component/user-form.component.js file

---
+++ src/component/user-form.component.js
@@ -0,0 +1,13 @@
+import React, { Component }  from "react";
+export default class UserForm extends Component {
+    render() {
+        return (
+            <form>
+                {/* <div className="form-group">
+                    <label>Name</label>
+
+                </div> */}
+            </form>
+        );
+    }
+}
